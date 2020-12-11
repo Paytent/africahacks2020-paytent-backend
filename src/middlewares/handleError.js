2a579a0a7e980b6e89a37a6ba7fd1e7f070dc3c0
Yusuf Abdulrahman <abdulrahmanyusuf125@gmail.com>
2020-12-11 12:47:38 +0100

    Update handleError.js

--- src/middlewares/handleError.js
+++ src/middlewares/handleError.js
@@ -1,7 +1,7 @@
 import { response } from "../utils/index.js";
 
 const handleError = (err, req, res, next) => {
-	res.status(err.statusCode || 500).json(response(err.message, null, false));
+	res.status(err.statusCode || 500).json(response(err.message, {}, false));
 };
 
 export default handleError;
