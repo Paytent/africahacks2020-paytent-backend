958b19c126972a604550f114db3355eccb3ed368
devYusuf <abdulrahmanyusuf125@gmail.com>
2020-12-11 22:47:05 +0100

    get transactions by wallet

--- docs/api_project.js
+++ docs/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-12-11T21:46:21.791Z",
+    "time": "2020-12-11T21:47:00.309Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
